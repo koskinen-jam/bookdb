8fe79d62050908fbe40b02ede51feb13b0880243
Janne Koskinen <23258606+koskinen-jam@users.noreply.github.com>
2022-08-22 01:18:57 +0300

    Parse example requests into tests

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -2,20 +2,92 @@ const fs = require('fs');
 
 // Parse an example request-response into an object with case name, request and
 // response strings
+//
+// --------------Example:---------
+// # Post a new book
+// POST http://localhost:9000/books
+// Content-Type: application/json
+//
+// {
+//         "title": "Harry Potter and the Philosophers Stone",
+//         "author": "J.K. Rowling",
+//         "year": 1997,
+//         "publisher": "Bloomsbury (UK)",
+//         "description": "A book about a wizard boy"
+// }
+//
+//
+// # Response
+// 200 OK
+//
+// {
+//         "id": 1
+// }
+//
+// -----------/Example-----------
+
 const requestResponseParser = (s) => {
+	let r = {
+		case: '',
+		request: {},
+		response: {},
+	};
 	let lines = s.split("\n");
 
-	let caseName = lines.shift().slice(2);
+	// Test case name
+	r.case = lines.shift().slice(2);
 
-	let splitAt = lines.findIndex(ln => ln === '# Response');
+	// Split off request parts
+	let splitIdx = lines.findIndex(ln => ln === '# Response');
+	let request = lines.splice(0, splitIdx - 1);
+	let headers = {};
+	let line;
 
-	return {
-		case: caseName,
-		request: lines.slice(0, splitAt).join("\n"),
-		response: lines.slice(splitAt + 1).join("\n"),
-	};
+	// Request HTTP method, URL and headers:
+	// Separated from body / response by empty line
+	while ((line = request.shift())) {
+		let parts = line.split(' ');
+		if (['GET', 'POST', 'DELETE'].includes(parts[0])) {
+			r.request.method = parts[0];
+			r.request.uri = parts[1];
+			r.request.path = parts[1].replace('http://localhost:9000', '');
+			continue;
+		}
+
+		headers[parts[0].replace(':', '')] = parts[1];
+	}
+	r.request.headers = headers;
+
+	// Request body
+	try {
+		r.request.body = JSON.parse(request.join("\n"));
+	} catch (e) {
+		// include unparseable JSON bodies as strings
+		r.request.body = request.join("\n");
+	}
+
+	// discard whitespace and '# Response'
+	let response = lines.splice(2);
+
+	// Response code and message
+	line = response.shift();
+
+	let m = line.match(/^(\d{3}) (.*)$/);
+
+	r.response.status = parseInt(m[1]);
+	r.response.message = m[2];
+
+	// Response body
+	try {
+		r.response.body = JSON.parse(response.join("\n"));
+	} catch (e) {
+		r.response.body = response.join("\n");
+	}
+
+	return r;
 };
 
+// Parse example requests
 const requestFile = 'test/requests.txt';
 
 const cases = fs.readFileSync(requestFile, 'utf-8')
@@ -23,3 +95,36 @@ const cases = fs.readFileSync(requestFile, 'utf-8')
 	.map(requestResponseParser);
 
 test('Loaded test cases', () => { expect(cases.length).toBeGreaterThan(0); });
+
+// Test server responses
+const request = require('supertest');
+
+const app = require('../src/app.js');
+
+for (let c of cases) {
+	test(c.case, async () => {
+		let res;
+		switch (c.request.method) {
+			case 'DELETE':
+				res = await request(app).delete(c.request.path);
+				break;
+			case 'GET':
+				res = await request(app).get(c.request.path);
+				break;
+			case 'POST':
+				res = await request(app)
+					.post(c.request.path)
+					.set(
+						'Content-Type',
+						c.request.headers['Content-Type']
+					)
+					.send(c.request.body);
+				break;
+			default:
+				break;
+		}
+
+		expect(res.status).toEqual(c.response.status);
+		expect(res.body).toEqual(c.response.body);
+	});
+}

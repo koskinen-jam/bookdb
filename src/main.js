d4b5767b2dc0b24a4bdf17a9edaab84d6a2614f2
Janne Koskinen <23258606+koskinen-jam@users.noreply.github.com>
2022-08-23 23:41:18 +0300

    Make the server functional
     * Add optional command-line arguments for server port and database file
       path with defaults. (And --help just for completeness's sake.)
     * Running main.js initializes the api and starts listening to given
       port.
     * Add signal handlers and shutdown handler

--- src/main.js
+++ src/main.js
@@ -1,5 +1,60 @@
-const app = require('./app.js');
+const args = require('minimist')(process.argv.slice(2));
 
-const port = 9000;
+if (args.help) {
+	console.log(`
+Books api server
 
-app.listen(port, () => console.log(`Listening at port ${port}.`));
+Usage
+
+	node src/main.js [--port=<int>] [--database=<path>] [--help]
+
+	or
+
+	npm run server -- [--port=<int>] [--database=<path>] [--help]
+
+Arguments
+
+	port=<int>
+
+		Set the port the server listens to on localhost. Default 9000.
+
+	database=<path>
+
+		Set the database file path. Default 'books.db'. Use ':memory:'
+		for a temporary db.
+
+	--help
+
+		Print help and exit without further actions.
+`);
+	process.exit(0);
+}
+
+const port = args.port ?? 9000;
+
+const dbpath = args.database ?? 'books.db';
+
+if (dbpath.match(/^\/.*/) || dbpath.match(/\.\./)) {
+	console.log('Database path must not start with "/" or contain "..".');
+	process.exit(1);
+}
+
+const { DB } = require('./database.js');
+const db = new DB(dbpath);
+const app = require('./app.js').init(db);
+
+const server = app.listen(port, () => console.log(`Listening at port ${port}.`));
+
+process.on('exit', (code) => {
+	console.log('Cleaning up.');
+	server.close(() => {
+		console.log('Stopped listening.');
+	});
+	db.close();
+	console.log('Closed database.');
+	console.log(`Exiting with code ${code}.`);
+});
+process.on('SIGHUP', () => process.exit(128 + 1));
+process.on('SIGINT', () => process.exit(128 + 2));
+process.on('SIGTERM', () => process.exit(128 + 15));
+
